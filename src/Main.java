2dadbf713936fb9c79941042c4627b0345bc2b40
Fabian Melendez <fabianmelendez321@gmail.com>
2025-02-20 22:15:42 -0500

    cambio2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Fabian Guillermo Melendez Gaitan");
         System.out.println("Ingenieria de sistemas y computacion");
-        System.out.println("jijijijujuju");
+        System.out.println("jijijujuju");
         }
     }
